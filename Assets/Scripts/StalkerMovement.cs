a7a2357bde4e1ff70f4745b25da04857b4d7a72f
JackC <jackc0022@gmail.com>
2019-04-30 20:30:39 -0700

    fixed compile error with StalkerMovement script

--- Assets/Scripts/StalkerMovement.cs
+++ Assets/Scripts/StalkerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StalkerDetect : MonoBehaviour
+public class StalkerMovement : MonoBehaviour
 {
     public float smooth, angle;
     private Quaternion targetRotation;

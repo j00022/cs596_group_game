371d6e0003404700640b3f11efd64ddf692181d0
JackC <jackc0022@gmail.com>
2019-05-04 20:21:18 -0700

    Enemies will continue to chase player for x seconds after losing sight of player

--- Assets/Scripts/StalkerMovement.cs
+++ Assets/Scripts/StalkerMovement.cs
@@ -5,9 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class StalkerMovement : MonoBehaviour {
 
-    public float speed, visionRange, visionRadius, minDist;
+    public float speed, visionRange, visionRadius, personalSpace, minDist, chaseCounter, turnCounter;
     public int turnDir;
-    private float currentHitDistance, turnCounter;
+    private float currentHitDistance;
     private int x;
     private GameObject player;
     private Vector3 fwd, origin;
@@ -16,14 +16,15 @@ public class StalkerMovement : MonoBehaviour {
     void Start() {
         player = GameObject.FindWithTag("Player");
         minDist = 2f;
-        speed = (Random.Range(1.75f, 2.5f));
+        speed = (Random.Range(1.75f, 2.25f));
         visionRange = Mathf.Infinity;
         visionRadius = 0.7f;
         currentHitDistance = visionRange;
         turnDir = (Random.Range(0, 2) == 0) ? 1 : -1;
         turnCounter = 0;
-        GetComponent<Rigidbody>().isKinematic = true;
-        x = (Random.Range(9, 12));
+        GetComponent<Rigidbody>().isKinematic = false;
+        x = (Random.Range(9, 15));
+        chaseCounter = 0;
     }
 
     void Update() {
@@ -31,18 +32,20 @@ public class StalkerMovement : MonoBehaviour {
         fwd = transform.TransformDirection(Vector3.forward);
         /*Change turn direction every x seconds*/
         turnCounter += 1 * Time.deltaTime;
-        if (turnCounter % x < 1) {
+        if (Mathf.FloorToInt(turnCounter) % x < 1) {
             turnDir *= -1;
-            x = (Random.Range(9, 12));
+            x = (Random.Range(9, 15));
+            if (turnCounter > 100)
+                turnCounter = 0;
         }
         if (Physics.SphereCast(origin, visionRadius, fwd, out hit, visionRange)) {
             currentHitDistance = hit.distance;
             if (hit.collider.gameObject.tag == "Player") {
+                chaseCounter = 2;
                 Chase(player);
             }
-            else if (currentHitDistance <= minDist) {
-                AvoidWall(turnDir);
-            }
+            else if (currentHitDistance <= minDist)
+                AvoidWall();
             else
                 transform.position += transform.forward * Time.deltaTime * speed;
         }
@@ -50,17 +53,22 @@ public class StalkerMovement : MonoBehaviour {
             transform.position += transform.forward * Time.deltaTime * speed;
             currentHitDistance = visionRange;
         }
+        /*Chases player for chaseCounter seconds after losing sight*/
+        if (chaseCounter >= 0) {
+            Chase(player);
+            chaseCounter -= 1 * Time.deltaTime;
+        }
     }
 
     void Chase(GameObject player) {
         Vector3 direction = player.transform.position - transform.position;
         Quaternion newDir = Quaternion.LookRotation(direction);
         transform.rotation = Quaternion.Lerp(transform.rotation, newDir, 20 * Time.deltaTime);
-        transform.position += transform.forward * Time.deltaTime * speed * 1.5f;
+        transform.position += transform.forward * Time.deltaTime * speed * 1.25f;
     }
 
-    public void AvoidWall(int direction) {
-        Quaternion degrees = Quaternion.Euler(0, direction * speed, 0);
+    public void AvoidWall() {
+        Quaternion degrees = Quaternion.Euler(0, turnDir * speed, 0);
         Quaternion turn = transform.rotation *= degrees;
         transform.rotation = Quaternion.Lerp(transform.rotation, turn, Time.deltaTime);
     }

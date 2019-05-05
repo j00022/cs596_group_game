f09bf99cdcdf27caab33f520bb1a186e5cb4ad20
JackC <jackc0022@gmail.com>
2019-05-04 18:13:59 -0700

    Randomized enemy movement speed, increases if chasing player. Enemies can now turn left if about to hit a wall

--- Assets/Scripts/StalkerMovement.cs
+++ Assets/Scripts/StalkerMovement.cs
@@ -5,37 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class StalkerMovement : MonoBehaviour {
 
-    public float speed, range, radius, minDist;
-    private float currentHitDistance;
+    public float speed, visionRange, visionRadius, minDist;
+    public int turnDir;
+    private float currentHitDistance, turnCounter;
+    private int x;
     private GameObject player;
     private Vector3 fwd, origin;
     RaycastHit hit;
 
     void Start() {
         player = GameObject.FindWithTag("Player");
-        minDist = 3f;
-        speed = 2f;
-        range = Mathf.Infinity;
-        radius = 0.7f;
-        currentHitDistance = range;
+        minDist = 2f;
+        speed = (Random.Range(1.75f, 2.5f));
+        visionRange = Mathf.Infinity;
+        visionRadius = 0.7f;
+        currentHitDistance = visionRange;
+        turnDir = (Random.Range(0, 2) == 0) ? 1 : -1;
+        turnCounter = 0;
+        GetComponent<Rigidbody>().isKinematic = true;
+        x = (Random.Range(9, 12));
     }
 
     void Update() {
         origin = transform.position;
         fwd = transform.TransformDirection(Vector3.forward);
-        if (Physics.SphereCast(origin, radius, fwd, out hit, range)) {
+        /*Change turn direction every x seconds*/
+        turnCounter += 1 * Time.deltaTime;
+        if (turnCounter % x < 1) {
+            turnDir *= -1;
+            x = (Random.Range(9, 12));
+        }
+        if (Physics.SphereCast(origin, visionRadius, fwd, out hit, visionRange)) {
             currentHitDistance = hit.distance;
             if (hit.collider.gameObject.tag == "Player") {
                 Chase(player);
             }
-            else if (hit.collider.gameObject.tag != "Stalker" && currentHitDistance <= minDist)
-                AvoidWall();
+            else if (currentHitDistance <= minDist) {
+                AvoidWall(turnDir);
+            }
             else
                 transform.position += transform.forward * Time.deltaTime * speed;
         }
         else {
             transform.position += transform.forward * Time.deltaTime * speed;
-            currentHitDistance = range;
+            currentHitDistance = visionRange;
         }
     }
 
@@ -43,11 +56,11 @@ public class StalkerMovement : MonoBehaviour {
         Vector3 direction = player.transform.position - transform.position;
         Quaternion newDir = Quaternion.LookRotation(direction);
         transform.rotation = Quaternion.Lerp(transform.rotation, newDir, 20 * Time.deltaTime);
-        transform.position += transform.forward * Time.deltaTime * speed;
+        transform.position += transform.forward * Time.deltaTime * speed * 1.5f;
     }
 
-    public void AvoidWall() {
-        Quaternion degrees = Quaternion.Euler(0, speed, 0);
+    public void AvoidWall(int direction) {
+        Quaternion degrees = Quaternion.Euler(0, direction * speed, 0);
         Quaternion turn = transform.rotation *= degrees;
         transform.rotation = Quaternion.Lerp(transform.rotation, turn, Time.deltaTime);
     }
@@ -62,6 +75,6 @@ public class StalkerMovement : MonoBehaviour {
     private void OnDrawGizmos() {
         Gizmos.color = Color.red;
         Debug.DrawLine(origin, origin + fwd * currentHitDistance);
-        Gizmos.DrawWireSphere(origin + fwd * currentHitDistance, radius);
+        Gizmos.DrawWireSphere(origin + fwd * currentHitDistance, visionRadius);
     }
 }
